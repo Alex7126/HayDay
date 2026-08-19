94e48e5b43fd2f31b1f32e4db1c620f9d81b90a5
Alex7126 <65097651+Alex7126@users.noreply.github.com>
2026-08-19 00:41:56 -0600

    Add products data and configuration settings

---
+++ products.js
@@ -0,0 +1,32 @@
+// products.js
+const PRODUCTS_DATA = [
+  ["langosta", "🦞 Langosta", 1000],
+  ["pescado", "🐟 Pescado", 1000],
+  ["miel", "🍯 Miel", 1000],
+  ["nata", "🥛 Nata", 1000],
+  ["mantequilla", "🧈 Mantequilla", 1000],
+  ["queso_vaca", "🧀 Queso de vaca", 1000],
+  ["queso_cabra", "🐐 Queso de cabra", 1000],
+  ["leche", "🥛 Leche", 1000],
+  ["huevo", "🥚 Huevo", 1000],
+  ["beicon", "🥓 Beicon", 1000],
+  ["pan", "🍞 Pan", 1000],
+  ["pan_maiz", "🌽 Pan de maíz", 1000],
+  ["tela", "🧵 Tela", 1000],
+  ["sierras", "🪚 Sierras", 1000],
+  ["hachas", "🪓 Hachas", 1000],
+  ["tnt", "💣 TNT", 1000],
+  ["dinamita", "🧨 Dinamita", 1000],
+  ["palas", "⛏️ Palas", 1000],
+  ["azucar_morena", "🟤 Azúcar morena", 1000],
+  ["azucar_blanca", "⚪ Azúcar blanca", 1000],
+  ["almibar", "🍯 Almíbar", 1000],
+  ["galletas", "🍪 Galletas", 1000],
+  ["especial", "⭐ Producto especial", 1000]
+];
+
+const CONFIG = {
+  adminWhatsApp: "527221017160",
+  neighborhoods: ["Crueles", "Dráculas"],
+  storageKey: "hayDayShopV2"
+};
